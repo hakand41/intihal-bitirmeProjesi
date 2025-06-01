ddd28da9738f580adec0050d4dbadecfbcff1187
hakand41 <degerhakan41@gmail.com>
2025-06-01 14:33:39 +0300

    config

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ CELERY_INCLUDE = ['app']
 
 # Veritabanı Yapılandırması
 DB_CONFIG = {
-    'server': '', # SQL Server örneğiniz
-    'database': '',                   # Veritabanı adınız
-    'username': '',     # SQL Server kullanıcı adınız (LÜTFEN GERÇEK KULLANICI ADINIZLA DEĞİŞTİRİN)
-    'password': '',           # SQL Server şifreniz (LÜTFEN GERÇEK ŞİFRENİZLE DEĞİŞTİRİN)
+    'server': 'DESKTOP-GT2ME32\\MSSQLSERVER03', # SQL Server örneğiniz
+    'database': 'OrjinalIntihalDb',                   # Veritabanı adınız
+    'username': 'yeni_kullanici_adiniz',     # SQL Server kullanıcı adınız (LÜTFEN GERÇEK KULLANICI ADINIZLA DEĞİŞTİRİN)
+    'password': 'GucLuP@rol@2025',           # SQL Server şifreniz (LÜTFEN GERÇEK ŞİFRENİZLE DEĞİŞTİRİN)
     'driver': '{ODBC Driver 17 for SQL Server}'
     # Eğer standart olmayan bir port kullanıyorsanız, buraya 'port': 1433 gibi ekleyebilirsiniz.
     # 'port': 1433 # Varsayılan port, gerekirse değiştirin
